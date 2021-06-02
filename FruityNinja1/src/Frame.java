4b2b24fd10da2b1223614954b1ee26b102b15d1b
aweso <aweso@DESKTOP-AFEMMQ7.lan>
2021-06-02 13:19:15 -0700

    adding object

--- FruityNinja1/src/Frame.java
+++ FruityNinja1/src/Frame.java
@@ -24,7 +24,9 @@ public class Frame extends JPanel implements ActionListener, MouseListener {
 	CutWatermelon cutwatermelon = new CutWatermelon();
 	Bomb bomb = new Bomb();
 	long count = 0;
-	
+
+	Music bang = new Music("bababooey.wav",false);
+Music point = new Music("androidsound.wav",false);
 	
 	public void paint(Graphics g) {
 	super.paintComponent(g);	
