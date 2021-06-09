233057f669b85acf8a13e590fa54bcbf1153be67
aweso <aweso@DESKTOP-AFEMMQ7.lan>
2021-06-08 22:52:16 -0700

    Added game over if you hit bomb

--- FruityNinja1/src/Frame.java
+++ FruityNinja1/src/Frame.java
@@ -26,11 +26,14 @@ public class Frame extends JPanel implements ActionListener, MouseListener, Mous
 	Banana[] ba = new Banana[100];
 	Kiwi[] k = new Kiwi[100];
 	
+	boolean hi=false;
+	
 	int c1,c2,c3,c4,c5,c6;
 	int q,w,e,r,t,y = 0;
 	
 	int p1Score = 0;
 	Font verdana = new Font("Verdana", Font.BOLD,40);
+	Font lose = new Font("Verdana", Font.BOLD,100);
 
 	Music bang = new Music("bababooey.wav",false);
 	Music point = new Music("androidsound.wav",false);
@@ -132,10 +135,16 @@ public class Frame extends JPanel implements ActionListener, MouseListener, Mous
 			k[i].paint(g);
 			
 			//score
-			g.setFont(verdana);//set the font
+			
+			//while(hi==false) {
+				g.setFont(verdana);//set the font
 			   //drawing text on the screen + using variables
-			   g.drawString(""+p1Score, 150,100);		   
-			 	
+			   g.drawString(""+p1Score, 150,100);	
+			//}
+			 if(hi==true) {
+				 g.setFont(lose);//set the font
+				 g.drawString("Game Over", 150,250);	
+			 }
 			   
 		}
 	}
@@ -220,10 +229,10 @@ public class Frame extends JPanel implements ActionListener, MouseListener, Mous
 		int mX =arg0.getX();
 		int mY =arg0.getY();
 		for(int i = 0; i<100;i++) {
-//			if(b[i].collide(mX, mY)) {
-//				//point.play();
-			//p1Score+=100;
-//			}
+			if(b[i].collide(mX, mY)) {
+				//point.play();
+			hi=true;
+		}
 			if(m[i].collide(mX, mY)) {
 				//point.play();
 				p1Score+=10;
@@ -245,9 +254,10 @@ public class Frame extends JPanel implements ActionListener, MouseListener, Mous
 				p1Score+=50;
 			}
 			if(m[i].collide(mX, mY)) {
-				//bomp play or something
+				
 			}
 		}
+		
 	}
 	
 	@Override
