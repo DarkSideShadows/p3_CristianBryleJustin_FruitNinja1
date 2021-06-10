1bd732c679b4e1ea6bf5fa6f77b2aabaf4195a9c
bryle <bryle@LAPTOP-JF4E44O7.san.rr.com>
2021-06-10 12:22:41 -0700

    l

--- FruityNinja1/src/Ground.java
+++ FruityNinja1/src/Ground.java
@@ -9,6 +9,7 @@ public class Ground {
 	private int x = 0,y = 0;
 	private Image img; // image of the frog
 	private AffineTransform tx = AffineTransform.getTranslateInstance(x, y);
+	boolean en = false;
 
 	public Ground() {
 		//the filename to the file is referenced here for the bg image
@@ -21,8 +22,12 @@ public class Ground {
 		g2.drawImage(img, tx, null);
 		
 	}
-	
-	
+	public boolean getEn() {
+		return en;
+	}
+	public void setEn(boolean pen) {
+		en = pen;
+	}
 	private void init(double a, double b) {
 		tx.setToTranslation(a, b);
 		tx.scale(.46666,.52);
